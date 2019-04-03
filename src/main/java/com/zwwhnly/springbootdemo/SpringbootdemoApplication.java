16c2c6a4a194f5f0f19f9a33f79b50b3724ab3e9
zwwhnly <zwwhnly@163.com>
2019-04-03 14:33:54 +0800

    添加项目初始文件

---
+++ src/main/java/com/zwwhnly/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,21 @@
+package com.zwwhnly.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    @RequestMapping
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+}
+
