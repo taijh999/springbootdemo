4c9955bd724b210d0556a5b7f3a9d6a9a5eb447d
zwwhnly <zwwhnly@163.com>
2019-04-08 10:50:47 +0800

    获取配置文件值

--- src/main/java/com/zwwhnly/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/zwwhnly/springbootdemo/SpringbootdemoApplication.java
@@ -1,5 +1,7 @@
 package com.zwwhnly.springbootdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,29 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
+    @Value("${book.author}")
+    private String bookAuthor;
+    @Value("${book.name}")
+    private String bookName;
+
+    @Autowired
+    private AuthorSettings authorSettings;
+
     @RequestMapping
     public String hello() {
         return "Hello Spring Boot!";
     }
+
+    @RequestMapping("/")
+    public String index() {
+        return "book name is:" + bookName + " and book author is:" + bookAuthor;
+    }
+
+    @RequestMapping("/indexV2")
+    public String indexV2() {
+        return "author name is:" + authorSettings.getName() + " and author age is:" + authorSettings.getAge();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
