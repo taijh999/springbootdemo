f4ad6043948956a067928e6598280dbfe1fa6000
zwwhnly <zwwhnly@163.com>
2019-04-18 16:54:14 +0800

    定时任务深入理解

--- src/main/java/com/zwwhnly/springbootdemo/TestSchedule.java
+++ src/main/java/com/zwwhnly/springbootdemo/TestSchedule.java
@@ -1,14 +1,22 @@
 package com.zwwhnly.springbootdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class TestSchedule {
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private List<Integer> index = Arrays.asList(8 * 1000, 3 * 1000, 6 * 1000, 2 * 1000, 2 * 1000);
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
 
     // 每5秒执行一次
     /*@Scheduled(fixedRate = 5000)
@@ -32,13 +40,68 @@ public class TestSchedule {
         }
     }*/
 
-    @Scheduled(cron = "0/5 * * * * ?")
+    /*@Scheduled(cron = "0/5 * * * * ?")
     public void testCron() {
         System.out.println("当前时间：" + simpleDateFormat.format(new Date()));
         try {
             Thread.sleep(6000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }*/
+
+    /*@Scheduled(fixedRate = 5 * 1000)
+    public void fixedRate() throws Exception {
+        int i = atomicInteger.get();
+        if (i < 5) {
+            Integer sleepTime = index.get(i);
+            logger.info("第{}个任务开始执行,执行时间为{}ms", i, sleepTime);
+            Thread.sleep(sleepTime);
+            atomicInteger.getAndIncrement();
+        }
+    }*/
+
+    /*@Scheduled(cron = "0/5 * * * * ? ")
+    public void cron() throws Exception {
+        int i = atomicInteger.get();
+        if (i < 5) {
+            Integer sleepTime = index.get(i);
+            logger.info("第{}个任务开始执行,执行时间为{}ms", i, sleepTime);
+            Thread.sleep(sleepTime);
+            atomicInteger.getAndIncrement();
+        }
+    }*/
+
+    /*@Scheduled(fixedDelay = 5 * 1000)
+    public void testFixedDelay() throws Exception {
+        int i = atomicInteger.get();
+        if (i < 5) {
+            Integer sleepTime = index.get(i);
+            System.out.println("当前时间：" + simpleDateFormat.format(new Date()));
+            Thread.sleep(sleepTime);
+            atomicInteger.getAndIncrement();
+        }
+    }*/
+
+    /*@Scheduled(fixedRate = 5000)
+    public void testFixedRate() throws Exception {
+        int i = atomicInteger.get();
+        if (i < 5) {
+            Integer sleepTime = index.get(i);
+            System.out.println("当前时间：" + simpleDateFormat.format(new Date()));
+            Thread.sleep(sleepTime);
+            atomicInteger.getAndIncrement();
+        }
+    }*/
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void testCron() throws Exception {
+        int i = atomicInteger.get();
+        if (i < 5) {
+            Integer sleepTime = index.get(i);
+            System.out.println("当前时间：" + simpleDateFormat.format(new Date()));
+            Thread.sleep(sleepTime);
+            atomicInteger.getAndIncrement();
         }
     }
 }
